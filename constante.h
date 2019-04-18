b4286eecdf735d37032c41dab5da2b69d278fe92
IbrahimLakhzine <46730171+IbrahimLakhzine@users.noreply.github.com>
2019-04-18 17:47:18 +0200

    header added

---
+++ constante.h
@@ -0,0 +1,29 @@
+#define largeur_window 900
+#define hauteur_window 600
+
+
+
+
+#include <stdio.h>
+#include<stdlib.h>
+#include <SDL.h>
+#include <string.h>
+#include <SDL_ttf.h>
+#include "insert_car.c"
+#include "detect_case_easy.c"
+#include "detect_case_easy_save.c"
+#include "detect_case_medium.c"
+#include "detect_case_medium_save.c"
+#include "detect_case_hard.c"
+#include "detect_case_hard_save.c"
+#include "players.c"
+#include "solver.c"
+//#include "top_scores.c"
+#include "menu.h"
+#include "login.c"
+
+
+
+
+
+
